341455a7d5e6ef4b2453434c3a2fe7682cb7c3e9
JoAPSo Paulo Gomes Torres Abelha <up201706412@fe.up.pt>
2020-03-21 13:28:52 +0000

    created SA code structure

--- Project/src/main/java/SA.java
+++ Project/src/main/java/SA.java
@@ -3,7 +3,9 @@ public class SA extends Algorithm {
 
 
     //Set initial temp
-    double temperature = 100000;
+    final double TEMPERATURE_INITIAL = 100000;
+
+    double temperature;
 
     //Cooling rate
     double coolingRate = 0.003;
@@ -13,33 +15,43 @@ public class SA extends Algorithm {
         super();
     }
 
-    public static double acceptanceProbability(int currentDistance, int newDistance, double temperature) {
-        // If the new solution is better, accept it
-        if (newDistance < currentDistance) {
-            return 1.0;
-        }
-        // If the new solution is worse, calculate an acceptance probability
-        return Math.exp((currentDistance - newDistance) / temperature);
-    }
+    @Override
+    public int[][] getNextState(int[][] state) {
+        // insertion
+        // swap type1 and swap type2
 
+        return null;
+    }
 
     @Override
     public void solve() {
+        temperature = TEMPERATURE_INITIAL;
 
-        System.out.println("Search Annealing algorithm");
+        int[][] currentState = super.state; // initial configuration
+        int evaluation = evaluate(currentState);
 
-
-        int currentSolution = 0;
-        int bestSolution = 0;
+        int currentSolution;
+        int bestSolution = evaluation;
 
 
         while(temperature > 1) {
 
-            // create a neighbour state
-            // calculate the new solution value
-            // check if we should accept it based on probability
-            // keep track of the best solution found
+            int [][] new_state = getNextState(currentState);
 
+            currentSolution = evaluate(new_state);
+
+            // if is better, always accept
+            if (currentSolution > bestSolution) {
+                bestSolution = currentSolution;
+                currentState = state;
+            }
+            else { // if is not better, accept based on probability
+                int delta = bestSolution - currentSolution;
+                double random = Math.random();
+                if (random <  Math.exp(- delta/ temperature)) {
+                    currentState = state;
+                }
+            }
 
             temperature *=  (1- coolingRate);
         }
@@ -47,8 +59,4 @@ public class SA extends Algorithm {
 
     }
 
-    @Override
-    public int[][] getNextState(int[][] state) {
-        return null;
-    }
 }
